7b89b45e78bd2a3304561456c1cc7c2f40b3563a
Joshua David Barillas <jbarillas87@gmail.com>
2026-04-02 14:02:33 -0400

    Add Pandas conversion for DataFrame display
    Converted Snowpark DataFrame to Pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages.
 import streamlit as st
 import requests  
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app.
@@ -16,7 +17,12 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
+pdf_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
