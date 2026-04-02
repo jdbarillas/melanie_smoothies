218bb4ec5ccfad937e91b6c924a573e186bb4342
Joshua David Barillas <jbarillas87@gmail.com>
2026-04-02 14:03:51 -0400

    Fix variable name from pdf_df to pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 #st.stop()
 
 # convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
-pdf_df = my_dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
